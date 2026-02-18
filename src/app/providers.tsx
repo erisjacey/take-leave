7e67252897cb38f23d83525244570e22b647646f
erisjacey-DESKTOP <root@erisjacey.dev>
2026-02-18 22:13:25 +0800

    feat: add root layout with theme provider and fonts

---
+++ src/app/providers.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { ThemeProvider } from 'next-themes'
+
+const Providers = ({ children }: { children: React.ReactNode }) => (
+  <ThemeProvider
+    attribute="class"
+    defaultTheme="system"
+    enableSystem
+    disableTransitionOnChange
+  >
+    {children}
+  </ThemeProvider>
+)
+
+export default Providers
